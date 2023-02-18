0a626e7dcef0730cd5e6ef9f4b58c8e8d08645da
mhaprean <marianhaprean@gmail.com>
2023-02-19 01:27:34 +0200

    fetch my tournaments on tournaments page

--- src/pages/MyTournaments.tsx
+++ src/pages/MyTournaments.tsx
@@ -1,16 +1,22 @@
+import Loader from '../components/Loader';
 import AddTournament from '../components/tournament/AddTournament';
 import TournamentList from '../components/tournament/TournamentList';
-import { useGetMyTournamentsAsHostQuery } from '../redux/apiSlice';
+import { useGetMyTournamentsAsHostQuery, useGetMyTournamentsQuery } from '../redux/apiSlice';
 import { useAppSelector } from '../redux/hooks';
 
 const MyTournaments = () => {
   const authState = useAppSelector((state) => state.auth);
 
   const { data: myTournamentsAsHost, isLoading } = useGetMyTournamentsAsHostQuery({}, { skip: authState.user?.role !== 'host' });
+  const { data: myTournaments, isFetching: isTournamentsFetching } = useGetMyTournamentsQuery({});
 
   return (
     <div className="container">
       {authState.isAuth && authState.user?.role === 'host' && <AddTournament />}
+
+      {isTournamentsFetching && <Loader />}
+
+      {!isTournamentsFetching && <TournamentList tournaments={myTournaments || []} />}
 
       <TournamentList tournaments={myTournamentsAsHost || []} asHost />
     </div>
